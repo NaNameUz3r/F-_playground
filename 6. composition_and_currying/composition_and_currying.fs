17775181cee5b744f81391ca75a2f17d07ec8be7
mr Nanders0n <owner@mail>
2022-04-03 09:02:09 +0300

    where is this syntax error?! ><

--- 6. composition_and_currying/composition_and_currying.fs
+++ 6. composition_and_currying/composition_and_currying.fs
@@ -1,16 +1,19 @@
 // 20.3.1
 let vat n x = (x * float(n) / 100.0) + x
 
-printfn "%f" (vat 5 100)
+// printfn "%f" (vat 5 100)
 
 // 20.3.2
 let unvat n x = x * (float(100) / float(100 + n))
 
+// printfn "%f" (unvat 5 105)
+
 // 20.3.3
 let rec min f = 
     let rec iterMin n = 
-        if (f n) = 0 then n
+        if f(n) = 0 then n
         else iterMin(n + 1)
     iterMin 1 
 
-
+// let testFunc n = 10 - n
+// printfn "%d" (min testFunc)
