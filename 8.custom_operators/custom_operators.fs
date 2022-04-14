3a46f37e280944a8b7bccd3e1192d7c8a283fcd1
mr Nanders0n <owner@mail>
2022-04-14 23:07:42 +0300

    rewrite 23.4.1, fix typo in 23.4.2

--- 8.custom_operators/custom_operators.fs
+++ 8.custom_operators/custom_operators.fs
@@ -3,21 +3,19 @@ let (.+.) x y =
     let (g1, s1, c1) = x
     let (g2, s2, c2) = y
     
-    total_copper = (c1 + c2)
+    let total_wallet = (g1 + g2, s1 + s2, c1 + c2)
     
-    if total_copper > 11 then
-        total_silver = (s1 + s2) + total_copper / 12
-        total_copper = total_copper % 12
-    else
-        total_silver = (s1 + s2)
+    let rec count_coppers = function
+     | (g, s, c) when c <= 11 -> (g, s, c)
+     | (g, s, c) -> count_coppers (g, s + 1, c - 12)
 
-    if total_silver > 19 then
-        total_gold = (g1 + g2) + total_silver / 19
-        total_silver = total_silver % 12
-    else
-        total_gold = (g1 + g2)
+    let rec count_silvers = function
+     | (g, s, c) when s <= 19 -> (g, s, c)
+     | (g, s, c) -> count_silvers (g + 1, s - 20, c)
 
-    (total_gold, total_silver, total_copper)
+    let copper_conversioned = count_coppers total_wallet
+    let converted_wallet = count_silvers copper_conversioned
+    converted_wallet
     
 // ---------------------------------------------
 
@@ -32,22 +30,25 @@ let (.-.) x y =
     let (g1, s1, c1) = more
     let (g2, s2, c2) = less
 
-    total_copper = (c1 - c2)
-    
-    if total_copper > 11 then
-        total_silver = (s1 - s2) + total_copper / 12
-        total_copper = total_copper % 12
-    else:
-        total_silver = (s1 - s2)
-    
-    if total_silver > 19 then
-        total_gold = (g1 - g2) + total_silver / 19
-        total_silver = total_silver % 19
-    else
-        total_gold = (g1 - g2)
+    let rec count_coppers = function
+     | (g, s, c) when c <= 11 -> (g, s, c)
+     | (g, s, c) -> count_coppers (g, s + 1, c - 12)
 
-    (total_gold, total_silver, total_copper)
+    let rec count_silvers = function
+     | (g, s, c) when s <= 19 -> (g, s, c)
+     | (g, s, c) -> count_silvers (g + 1, s - 20, c)
+
+    let bigger_copper_conversioned = count_coppers more
+    let bigger_converted_wallet = count_silvers bigger_copper_conversioned
     
+    let smaller_copper_conversioned = count_coppers less
+    let smaller_converted_wallet = count_silvers smaller_copper_conversioned
+
+    let (bigger_g, bigger_s, bigger_c) = bigger_converted_wallet
+    let (smaller_g, smaller_s, smaller_c) = smaller_converted_wallet
+    (bigger_g - smaller_g, bigger_s - smaller_s, bigger_c - smaller_c)
+
+
 // --------------------------------------------- 
     
 
@@ -70,4 +71,4 @@ let (.*) x y =
 let (./) x y = 
     let (a, b) = x
     let (c, d) = y
-    (a, b) .* (c / (c ** 2 + d ** 2), -d / (c ** 2, + d ** 2))
+    (a, b) .* (c / (c * c + d * d), -d / (c * c + d * d))
