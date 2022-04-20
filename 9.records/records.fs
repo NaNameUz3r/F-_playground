c41e4d440f739239d95b419d48a271bdf7294a76
mr Nanders0n <owner@mail>
2022-04-20 10:10:12 +0300

    there must be some trick

---
+++ 9.records/records.fs
@@ -0,0 +1,10 @@
+type TimeOfDay = { hours : int;  minutes : int; f :string}
+
+let (.>.) x y = x > y 
+
+// let m = { hours = 8; minutes = 30; f = "AM" }
+// let e = { hours = 9; minutes = 45; f = "PM" }
+
+// printfn "%b" (e .>. m = true)
+// printfn "%b" (m .>. m = false)
+
