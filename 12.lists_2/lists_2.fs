f39e83b67fc2700b653f83c39d214e5216102c85
IvanZ <78234165+NaNameUz3r@users.noreply.github.co>
2022-05-04 10:10:37 +0300

    rewrite on recursion 39.2 and 39.3, rewrite 39.5 according to task

--- 12.lists_2/lists_2.fs
+++ 12.lists_2/lists_2.fs
@@ -2,18 +2,23 @@
 let rmodd l = 
     let list_length = List.length l
     let odd_elements = [ for i in 1 .. 2 .. list_length -> l.Item(i)]
-    odd_elements
+    odd_elements  
 
 // 39.2
-let del_even l = 
-    let odds_only = List.filter (fun x -> x % 2 <> 0) l
-    odds_only
+// let del_even l = 
+//     let odds_only = List.filter (fun x -> x % 2 <> 0) l
+//     odds_only
+
+let rec del_even = function
+ | head::tail when head % 2 = 0 -> del_even(tail)
+ | head::tail -> head::del_even(tail)
+ | [] -> [] 
 
 // 39.3
-let rec multiplicity x xs = 
-    let only_x = List.filter (fun n -> n = xs ) x
-    let times = List.length only_x
-    times
+let rec multiplicity x xs = match xs with
+ | [] -> 0
+ | head::tail when head = x -> 1 + multiplicity x tail
+ | head::tail -> multiplicity x tail
 
 // 39.4
 let rec split = function
@@ -21,10 +26,10 @@ let rec split = function
  | x::y::n -> 
             let (odd, even) = split n
             (x::odd, y::even)
- | _ -> failwith "Input list should have even lengs"
+ | _ -> failwith "Input list should have even length"
+
 
 // 39.5
-let rec zip = function
- | ([x], [y]) -> [(x, y)]
- | (x::tail_x, y::tail_y) -> (x, y)::zip(tail_x, tail_y)
- | _ -> failwith "Both of input lists should have even lengths"
+let rec zip (xs1,xs2)= match (xs1,xs2) with
+ | ([],[]) -> []
+ | (xhead::xtail, yhead::ytail) -> (xhead,yhead)::zip(xtail,ytail)
