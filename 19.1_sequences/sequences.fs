25ff7f77d8367a9d5cd4ac6b6872641d14710cc9
IvanZ <78234165+NaNameUz3r@users.noreply.github.co>
2022-05-26 10:11:19 +0300

    secuences

---
+++ 19.1_sequences/sequences.fs
@@ -0,0 +1,22 @@
+// 49.5.1
+let even_seq = Seq.initInfinite (fun i  -> i * 2 + 2)
+printfn "%A" even_seq
+
+// 49.5.2
+let fac_seq = 
+    let rec findFactorial n =
+        match n with
+        | 0 -> 1
+        | 1 -> 1
+        | _ -> n * findFactorial(n-1)
+
+    Seq.initInfinite factorial
+
+// 49.5.3
+let seq_seq = 
+    let generateMirroredInt n =
+        match n with
+        | 0 -> 0
+        | n when n % 2 = 0 -> n / 2
+        | _ -> -n / 2 - 1
+    Seq.initInfinite generateMirroredInt
